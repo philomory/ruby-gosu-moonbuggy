93474439f9eacff643a877e57460f8258a427ba2
philomory <philomory@localhost>
2009-06-14 11:13:05 -0700

    Aliased added enum_cons alias for each_cons in 1.9, for consistency.

--- moonbuggy.rb
+++ moonbuggy.rb
@@ -3,6 +3,14 @@
 # A translation of Chipmunk's Moon Buggy tutorial code to ruby.
 
 require 'enumerator'
+
+# Added for consistency between 1.8 and 1.9. Just ignore it.
+unless [].respond_to?(:enum_cons)
+  module Enumerable
+    alias :enum_cons :each_cons
+  end
+end
+
 require 'gosu'
 require 'chipmunk_object'
 require 'chipmunk_adjust'
