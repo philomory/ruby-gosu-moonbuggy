665a51d328a71f137de30e6415f171c34ecce414
philomory <philomory@localhost>
2009-06-14 03:00:20 -0700

    Ugh. Convert tabs to spaces!

--- chipmunk_adjust.rb
+++ chipmunk_adjust.rb
@@ -10,50 +10,50 @@ module CP
   def self.vzero
     CP::Vec2.new(0.0,0.0)
   end
-	module Shape
-		class Circle
-			attr_reader :radius, :center
-			alias_method :orig_init, :initialize
-			def initialize(body, radius, center)
-				@radius, @center = radius, center
-				orig_init(body,radius,center)
-			end
-			def draw(window)
+  module Shape
+    class Circle
+      attr_reader :radius, :center
+      alias_method :orig_init, :initialize
+      def initialize(body, radius, center)
+        @radius, @center = radius, center
+        orig_init(body,radius,center)
+      end
+      def draw(window)
         window.draw_circle_shape(self)
       end
-		end
-	
-		class Segment
-			attr_reader :a, :b, :radius
-			alias_method :orig_init, :initialize
-			def initialize(body, a, b, radius)
-				@a, @b, @radius = a, b, radius
-				orig_init(body,a,b,radius)
-			end
-			def draw(window)
+    end
+  
+    class Segment
+      attr_reader :a, :b, :radius
+      alias_method :orig_init, :initialize
+      def initialize(body, a, b, radius)
+        @a, @b, @radius = a, b, radius
+        orig_init(body,a,b,radius)
+      end
+      def draw(window)
         window.draw_segment_shape(self)
       end
-		end
-		
-		class Poly
-			attr_reader :verts, :offset
-			alias_method :orig_init, :initialize
-			def initialize(body, verts, offset)
-				@verts, @offset = verts, offset
-				orig_init(body,verts,offset)
-			end
-			def draw(window)
+    end
+    
+    class Poly
+      attr_reader :verts, :offset
+      alias_method :orig_init, :initialize
+      def initialize(body, verts, offset)
+        @verts, @offset = verts, offset
+        orig_init(body,verts,offset)
+      end
+      def draw(window)
         window.draw_poly_shape(self)
       end
-		end
-	end
-	class Body
-	  def draw(window)
+    end
+  end
+  class Body
+    def draw(window)
       window.draw_vertex(self.p.x,self.p.y)
     end
   end
-	module Object
-	  def draw(window)
+  module Object
+    def draw(window)
       @chipmunk_objects.each do |obj|
         obj.draw(window) if obj.is_a?(Shape) || obj.is_a?(Body)
       end
